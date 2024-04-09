bb54be03ffc0e21db37696e623a082d435fd458a
Revant Tripathi <revanttripathi8@gmail.com>
2024-04-09 23:12:53 +0530

    done

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,7 @@
 const User = require("../model/user");
+const { Resend } = require('resend');
+
+const resend = new Resend('re_88qSVmQD_H1jt7zdB7wwF9AzHGjHSkMSP');
 
 const createNewUser = async (req, res) => {
     console.log(req.body);
@@ -6,14 +9,27 @@ const createNewUser = async (req, res) => {
   
     
     const userObject = { name, username, email, password, city };
+
+    resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: email,
+      subject: 'Account Created',
+      html: '<p>Congrats on creating your <strong>Dribble Account</strong>!</p>'
+    });
   
-    const user = await User.create(userObject);     // to insert data
-  
-    if (user) {
-      res.status(201).json({ message: `new user ${username} created` });
-    } else {
+    const user = await User.create(userObject).then(
+      (resp)=>{console.log(resp)
+        res.status(201).json({ message: `new user ${name} created` });
+
+      }
+    ).catch((err)=>{
+
+      console.log(err);
       res.status(400).json({ messgae: `Invalid user data received` });
-    }
+    });     
+    
+  
+   
   };
 
   module.exports = {  
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express= require("express")
 require("dotenv").config();
 const app = express()
+const cors = require("cors");
 const connectDB = require("./config/dbCon");
 //connection
 
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", require("./routes/userRoutes"));
 
